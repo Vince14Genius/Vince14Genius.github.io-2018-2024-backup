e3b4f44e766b5871517417fc396e33e0f418eb05
Vince14Genius <vince14genius@icloud.com>
2021-04-07 15:13:43 -0400

    Create nav.js

---
+++ scripts/nav.js
@@ -0,0 +1,28 @@
+"use strict"
+
+const universalNav = document.getElementByID("universal-nav")
+universalNav.innerHTML = `
+<a id="skip-to-main-button" href="#main-content">Skip to main content</a>
+
+<nav>
+  <div class="exterior-width-wrapper">
+    <div class="nav-left">
+      <a class="nav-link light-text" href="/">
+        <img class="nav-image" id="nav-profile-picture" alt="Profile" src="/images/profile.png">
+        <span class="nav-left-inner"> Vince14Genius</span>
+      </a>
+    </div>
+    <!--
+    <div class="nav-right" id="nav-right-button">
+      <a class="nav-link"><img class="nav-image" alt="Menu"></a>
+    </div>
+    -->
+    <ul class="nav-right nav-ul" id="nav-right-menu">
+      <li class="nav-item"><a class="nav-link light-text" href="/about">About</a>
+      <li class="nav-item"><a class="nav-link light-text" href="/projects">Projects</a>
+      <li class="nav-item"><a class="nav-link light-text link-disabled">Blog</a>
+      <li class="nav-item"><a class="nav-link" href="/site-map"><img class="nav-image" alt="Site Map" src="/images/search.svg"></a>
+    </ul>
+  </div>
+</nav>
+`
