8e75e69be0944477ad9f8ed77ebc9dccee0af061
Vince14Genius <vince14genius@icloud.com>
2021-04-07 15:39:34 -0400

    Create head.js

---
+++ scripts/head.js
@@ -0,0 +1,8 @@
+"use strict"
+
+document.getElementsByTag("head")[0].innerHTML += `
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover">
+<link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+<link rel="stylesheet" type="text/css" href="/styles.css">
+`
