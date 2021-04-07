768efc59ed316419e5e04ce0702a2dd6d8e4269a
Vince14Genius <vince14genius@icloud.com>
2021-04-07 15:43:52 -0400

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -3,8 +3,8 @@
 const universalFooter = document.getElementById("universal-footer")
 universalFooter.innerHTML = `
     <hr class="footer-hr">
-    <p class="footer-normal compact-line-height">Contact by email at <a class="footer-link" href="mailto:vince14genius@protonmail.com">vince14genius@protonmail.com</a>.
-    <p class="footer-normal compact-line-height"><a class="footer-link" href="/site-map">Site Map</a> ・ <a class="footer-link" href="/privacy-policy">Privacy Policy</a> ・ <a class="footer-link" href="/support">Support</a>
+    <p class="footer-normal">Contact by email at <a class="footer-link" href="mailto:vince14genius@protonmail.com">vince14genius@protonmail.com</a>.
+    <p class="footer-normal"><a class="footer-link" href="/site-map">Site Map</a> ・ <a class="footer-link" href="/privacy-policy">Privacy Policy</a> ・ <a class="footer-link" href="/support">Support</a>
     <hr class="footer-hr">
-    <p class="footer-light compact-line-height"><span class="footer-normal">Copyright</span> © Vince14Genius (Vincent C.)
+    <p class="footer-light"><span class="footer-normal">Copyright</span> © Vince14Genius (Vincent C.)
 `
