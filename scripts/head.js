cbf64ca55dd53b87d48cb8a9a8379028f4b7ea6d
Vince14Genius <vince14genius@icloud.com>
2021-04-07 15:44:48 -0400

    Update head.js

--- scripts/head.js
+++ scripts/head.js
@@ -1,6 +1,6 @@
 "use strict"
 
-document.getElementsByTag("head")[0].innerHTML += `
+document.getElementsByTagName("head")[0].innerHTML += `
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover">
 <link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
