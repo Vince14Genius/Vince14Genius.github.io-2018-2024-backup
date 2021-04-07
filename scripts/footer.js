c7c4c2361f3f76b66396e540f0a9eb3593dfc9e3
Vince14Genius <vince14genius@icloud.com>
2021-04-07 15:30:35 -0400

    Create footer.js

---
+++ scripts/footer.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const universalFooter = document.getElementById("universal-footer")
+universalFooter.innerHTML = `
+    <hr class="footer-hr">
+    <p class="footer-normal compact-line-height">Contact by email at <a class="footer-link" href="mailto:vince14genius@protonmail.com">vince14genius@protonmail.com</a>.
+    <p class="footer-normal compact-line-height"><a class="footer-link" href="/site-map">Site Map</a> ・ <a class="footer-link" href="/privacy-policy">Privacy Policy</a> ・ <a class="footer-link" href="/support">Support</a>
+    <hr class="footer-hr">
+    <p class="footer-light compact-line-height"><span class="footer-normal">Copyright</span> © Vince14Genius (Vincent C.)
+`
