dc4a3ef10d8bafef354df1cb5e9d1922a4ede675
Vince14Genius <vince14genius@protonmail.com>
2021-08-12 16:51:08 -0700

    removed inaccessible viewport settings

--- scripts/head.js
+++ scripts/head.js
@@ -2,7 +2,7 @@
 
 document.getElementsByTagName("head")[0].innerHTML += `
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover">
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 <link rel="icon" href="/favicon.ico">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
