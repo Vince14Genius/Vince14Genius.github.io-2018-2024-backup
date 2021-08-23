4fa32f637155b3d20dc4c884ee54fe5c6e204042
Vince14Genius <vince14genius@protonmail.com>
2021-08-22 17:04:41 -0700

    Update nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -19,7 +19,10 @@ universalNav.innerHTML = `
       </a>
     </div>
     <div class="nav-right" id="nav-right-button">
-      <a class="nav-link" onclick="toggleMenu()"><img class="nav-image" alt="Menu"></a>
+      <a class="nav-link" onclick="toggleMenu()">
+        <span class="menu-button-top"></span>
+        <span class="menu-button-bottom"></span>
+      </a>
     </div>
     <ul class="nav-right nav-ul" id="nav-right-menu">
       <li class="nav-item"><a class="nav-link light-text" href="/about">About</a>
