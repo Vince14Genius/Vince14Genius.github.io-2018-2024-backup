639e7a2b1c52699db483fe0df3dc3b92668c596c
Vince14Genius <vince14genius@protonmail.com>
2021-08-24 16:18:38 -0700

    Update nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -9,8 +9,7 @@ const universalNav = document.getElementById("universal-nav")
 universalNav.innerHTML = `
 <a id="skip-to-main-button" href="#main-content">Skip to main content</a>
 
-<nav>
-  <!-- Regular Menu -->
+<nav class="noselect">
   <div class="exterior-width-wrapper full-height">
     <div class="nav-left">
       <a class="nav-link light-text" href="/">
@@ -18,28 +17,35 @@ universalNav.innerHTML = `
         <span class="nav-left-inner"> Vince14Genius</span>
       </a>
     </div>
-    <div class="nav-right" id="nav-right-button">
-      <div class="nav-link" onclick="toggleMenu()">
-        <span class="menu-button-top"></span>
-        <span class="menu-button-bottom"></span>
-      </div>
-    </div>
+    
     <ul class="nav-right nav-ul" id="nav-right-menu">
       <li class="nav-item"><a class="nav-link light-text" href="/about">About</a>
       <li class="nav-item"><a class="nav-link light-text" href="/projects">Projects</a>
       <li class="nav-item"><a class="nav-link light-text link-disabled">Blog</a>
       <!--<li class="nav-item"><a class="nav-link" href="/site-map"><img class="nav-image" alt="Site Map" src="/images/search.svg"></a>-->
     </ul>
-  </div>
+    
+    <!-- Mobile Pulldown Menu -->
+    <div class="nav-right" id="nav-right-button">
+    
+      <!-- Menu Button -->
+      <input type="checkbox" class="menu-hidden-checkbox" onclick="toggleMenu()">
+      <div class="nav-link">
+        <span class="menu-button-top"></span>
+        <span class="menu-button-bottom"></span>
+      </div>
   
-  <!-- Pulldown Menu -->
-  <div class="exterior-width-wrapper" id="nav-pulldown-menu" style="display: none;">
-    <ul class="nav-right nav-ul">
-      <li class="nav-item"><a class="nav-link light-text" href="/about">About</a>
-      <li class="nav-item"><a class="nav-link light-text" href="/projects">Projects</a>
-      <li class="nav-item"><a class="nav-link light-text link-disabled">Blog</a>
-      <!--<li class="nav-item"><a class="nav-link" href="/site-map"><img class="nav-image" alt="Site Map" src="/images/search.svg"></a>-->
-    </ul>
+      <!-- The Pulldown Menu Itself -->
+      <div class="exterior-width-wrapper" id="nav-pulldown-menu" style="display: none;">
+        <ul class="nav-right nav-ul">
+          <li class="nav-item"><a class="nav-link light-text" href="/about">About</a>
+          <li class="nav-item"><a class="nav-link light-text" href="/projects">Projects</a>
+          <li class="nav-item"><a class="nav-link light-text link-disabled">Blog</a>
+          <!--<li class="nav-item"><a class="nav-link" href="/site-map"><img class="nav-image" alt="Site Map" src="/images/search.svg"></a>-->
+        </ul>
+      </div>
+      
+    </div>
   </div>
 </nav>
 `
